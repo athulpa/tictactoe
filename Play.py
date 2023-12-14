493755dcf9b288e1769a7bc97114c1575b8bc9c6
Athul Prakash <athulpa@gmail.com>
2023-12-15 00:40:44 +0530

    Filename fixes

--- Play.py
+++ Play.py
@@ -1,32 +1,52 @@
 
-
 from Board import TicTacToeBoard
-from Engine import MiniMaxEngine
-from Engine import TableBaseEngine
 from TableBase import getAvailableTableBases
+from Engine import TableBaseEngine
+from Engine import MiniMaxEngine
 
-def startSimpleGame():
-    #eng = MiniMaxEngine()
-    eng = makeTableBaseEngine()
+def startGame():
+    welcomeScreen()
     
-    tb = TicTacToeBoard()
+    settings = {
+                 "name1"   : "",
+                 "name2"   : "",
+                 "marker1" : "X",
+                 "marker2" : "O",
+                 "useTB"   : True
+               }
     
-    rsp = welcomeScreen()
+    rsp = getSettings(settings, enableQuit=True, tryCnt=0)
+    if(rsp == -1):
+        exitMessage()
+        return
+    
+    rsp = getPlayers(settings, enableQuit=True)
     if(rsp==-1):
-        print("\nExiting ...\n")
+        exitMessage()
         return
     else:
         (p1,p2) = rsp
-
-    print("\n\nLET'S BEGIN!\n")
+        
+    tb = TicTacToeBoard()
     
-    drawBoard(tb)
+    if('c' in (p1,p2)):
+        if(settings['useTB']):
+            eng = makeTableBaseEngine()
+        else:
+            eng = MiniMaxEngine()
+        
+    resp = input("\nPress ENTER to start the game ...")
+    if(resp.strip().lower()=='q'):
+        exitMessage()
+        return
+    
+    drawBoard(tb, settings, pre='\n')
     
     while(True):
         
         nextPlayer = (p1 if(tb.nextTurn==tb.Xmark) else p2)
         if(nextPlayer == 'u'):
-            mv = getInputMove(tb)
+            mv = getInputMove(tb, enableQuit=True)
             if(mv==-1):
                 break
         else:
@@ -36,10 +56,14 @@ def startSimpleGame():
         
         tb.move(mv)
         
-        drawBoard(tb)
+        drawBoard(tb, settings)
         
         if(tb.checkWin() != 0):
-            print("Player {} wins!".format('1' if(nextPlayer==p1) else '2'))
+            if(nextPlayer==p1):
+                winnerName = (settings['name1'] if(settings['name1']!='') else 'Player 1')
+            else:
+                winnerName = (settings['name2'] if(settings['name2']!='') else 'Player 2')
+            print(winnerName, "wins!")
             break
         
         if(len(tb.possibleNextMoves()) == 0):
@@ -47,7 +71,19 @@ def startSimpleGame():
             break
         
     print("\nGAME OVER\n")
-
+        
+    
+    
+def welcomeScreen(enableQuit  = True):
+    print("\nWELCOME TO TICTACTOE\n\n")
+    print("Enter 'q' at any time to quit.")
+    print()
+        
+def exitMessage():
+    print()
+    print("Exiting . . .")
+    print()
+    
 
 def makeTableBaseEngine():
     atb = getAvailableTableBases()
@@ -56,29 +92,111 @@ def makeTableBaseEngine():
     eng = TableBaseEngine(tbase)
     return eng
 
-def welcomeScreen(enableQuit=True):
-    print("\nWELCOME TO TICTACTOE\n\n")
-    print("PLAYER RULES")
-    print("The first player plays 'X' and moves first.\nThe second player plays 'O'.")
-    print()
-    print()
-    print("BOARD RULES")
-    print("The game will be played on a 3x3 board.")
-    print("Each cell has numbered co-ordinates of the form (row number, column number).")
-    print("Rows are numbered 1-3 from top to bottom; columns are numbered 1-3 from left to right.")
-    print("For instance, (1,1) is the top-left cell and (2,3) is the middle-right cell.")
-    print()
-    print()
-    print("To quit, type 'q' when asked for any input.")
-    print()
-    print()
-    _ = input("Press ENTER to continue ...")
-    print()
+
+def drawBoard(tb:TicTacToeBoard, settings, pre='\n', end='\n'):
+    symbol = {
+              0        :  " ",
+              tb.Xmark :  settings['marker1'],
+              tb.Ymark :  settings['marker2']
+              }
     
+    ret = pre
+    
+    dispName1 = (settings['name1'] if (settings['name1']!='') else "Player 1")
+    dispName2 = (settings['name2'] if (settings['name2']!='') else "Player 2")
+    toMoveLine = "Next: "
+    toMoveLine += (dispName1 if(tb.nextTurn==tb.Xmark) else dispName2)
+    toMoveLine += " (" + (settings['marker1'] if (tb.nextTurn==tb.Xmark) else settings['marker2']) + ")"
+    toMoveLine +=  '\n'
+    
+    for i in range(3):
+        for j in range(3):
+            ret += " " + symbol[tb.board[3*i+j]] + " |"
+        if(i!=2):
+            ret = ret[:-1] + "\n" + "-"*11 + "\n"
+        else:
+            ret = ret[:-1] + '\n'
+    
+    ret += '\n' + toMoveLine
+    ret += end
+    print(ret)
+    
+    
+def getSettings(settings, enableQuit = True, tryCnt = 0):
+    
+    while(True):
+        tryCnt += 1
+        
+        if(tryCnt==1):
+            print(" **** Default Settings ****")
+        else:
+            print(" **** Current Settings ****")
+            
+        print("Player 1's Name:", settings['name1'])
+        print("Player 2's Name:", settings['name2'])
+        print("Player 1's Marker:", settings['marker1'])
+        print("Player 2's Marker:", settings['marker2'])
+        print("Use a tablebase to speed up computer moves:", ('Yes' if settings['useTB'] else 'No'))
+        print()
+        resp = input("Change any of these settings? (Y to change): ")
+        if(enableQuit is True and resp.strip().lower()=='q'):
+            return -1
+    
+        
+        if(resp.strip().lower() == 'y'):
+            print()
+            print("Press ENTER to leave any setting unchanged")
+            print()
+            
+            resp = input("Player 1's Name (upto 20 characters): ").strip()
+            if(enableQuit is True and resp.lower()=='q'):
+                return -1
+            if(resp!=''):
+                settings['name1'] = resp[:20]
+            
+            resp = input("Player 2's Name (upto 20 characters): ").strip()
+            if(enableQuit is True and resp.lower()=='q'):
+                return -1
+            if(resp!=''):
+                settings['name2'] = resp[:20]
+            
+            resp = input("Player 1's Marker (1 character): ").strip()
+            if(enableQuit is True and resp.lower()=='q'):
+                return -1
+            if(resp!=''):
+                settings['marker1'] = resp[0]
+            
+            resp = input("Player 2's Marker (1 character): ").strip()
+            if(enableQuit is True and resp.lower()=='q'):
+                return -1
+            if(resp!=''):
+                settings['marker2'] = resp[0]
+            
+            resp = input("Computer uses a tablebase to speed up its moves (Y/n): ").strip()
+            if(enableQuit is True and resp.lower()=='q'):
+                return -1
+            settings['useTB'] = (resp.lower()!='n')
+            
+            print()
+            
+        else:
+            break
+    
+    return settings
+
+
+def getPlayers(settings, enableQuit = True):
+    dispName1 = settings['name1'] if settings['name1']!='' else "Player 1"
+    dispName1 += ("'s") if (dispName1[-1].lower() != 's') else ("'")
+        
+    dispName2 = settings['name2'] if settings['name2']!='' else "Player 2"
+    dispName2 += ("'s") if (dispName2[-1].lower() != 's') else ("'")
+        
     cnt = 0
     while(True):
         cnt += 1
-        r1 = input("\nWho plays first? ('u' for user and 'c' for computer): ")
+        
+        r1 = input("\nWho makes " + dispName1 + " moves?: ('u' for user and 'c' for computer): ")
         r1 = r1.strip().lower()
         if(enableQuit is True and r1=='q'):
             return -1
@@ -91,7 +209,7 @@ def welcomeScreen(enableQuit=True):
     cnt = 0
     while(True):
         cnt += 1
-        r2 = input("\nWho plays second? ('u' for user and 'c' for computer): ")
+        r2 = input("\nWho makes " + dispName2 + " moves?: ('u' for user and 'c' for computer): ")
         r2 = r2.strip().lower()
         if(enableQuit is True and r2=='q'):
             return -1
@@ -100,9 +218,9 @@ def welcomeScreen(enableQuit=True):
         else:
             print("Invalid Input. You must enter either 'u' or 'c'.")
             print("Try again ("+str(cnt)+")\n")
-            
+    
+    print()
     return r1,r2
-
 
 # Gets the next move from the user
 # If quit is enabled, returns -1 if the user enters 'q' to signal a game over
@@ -120,7 +238,7 @@ def getInputMove(tb, enableQuit=True):
             assert ((len(nums)==2) and (nums[0] in range(1,4)) and (nums[1] in range(1,4)))
             ret = 3*(nums[0]-1) + (nums[1]-1)
         except (AssertionError, ValueError, TypeError):
-            print("\nInvalid Input. Expected format: row-index <SPACE> column-index")
+            print("\nInvalid Input. Expected format: row <SPACE> column")
             print("Try again({})\n\n".format(cnt))
             continue
         else:
@@ -131,31 +249,5 @@ def getInputMove(tb, enableQuit=True):
                 continue
             
 
-def drawBoard(tb:TicTacToeBoard, Xchar='X', Ychar='O', pre='\n', end='\n'):
-    symbol = {
-              0        :  " ",
-              tb.Xmark :  Xchar,
-              tb.Ymark :  Ychar
-              }
-    
-    ret = pre
-    
-    toMoveLine = "Next: "
-    toMoveLine += (Xchar if(tb.nextTurn==tb.Xmark) else Ychar)
-    toMoveLine +=  '\n'
-    
-    for i in range(3):
-        for j in range(3):
-            ret += " " + symbol[tb.board[3*i+j]] + " |"
-        if(i!=2):
-            ret = ret[:-1] + "\n" + "-"*11 + "\n"
-        else:
-            ret = ret[:-1] + '\n'
-    
-    ret += '\n' + toMoveLine
-    ret += end
-    print(ret)
-    
-if __name__=='__main__':
-    startSimpleGame()
-    
+if __name__ == '__main__':
+    startGame()
